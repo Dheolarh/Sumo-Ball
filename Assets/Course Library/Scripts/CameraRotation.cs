9d120090dc94483178df8aec80f0656bb141f8e9
Dheolarh <adejuwonlookanalwon@gmail.com>
2024-11-03 11:57:54 +0100

    Movement

--- Assets/Course Library/Scripts/CameraRotation.cs
+++ Assets/Course Library/Scripts/CameraRotation.cs
@@ -5,14 +5,28 @@ using UnityEngine;
 public class CameraRotation : MonoBehaviour
 {
     public float rotationSpeed;
+    public Rigidbody ballRb;
+    public float pushForce;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ballRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        BallMovement();
+        CameraRotate();
+    }
+
+    void BallMovement()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        ballRb.AddForce(Vector3.forward * Time.deltaTime * pushForce * verticalInput, ForceMode.Impulse);
+    }
+
+    void CameraRotate()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         transform.Rotate(Vector3.up, rotationSpeed * horizontalInput);
